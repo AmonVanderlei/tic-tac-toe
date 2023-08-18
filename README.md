2fdeb514580fb80e9cbd1ba17ffdd5197c0833ca
Amon Vanderlei <amon.chalegre@gmail.com>
2023-08-18 20:46:06 -0300

    first commit

--- README.md
+++ README.md
@@ -1,70 +1,17 @@
-# Getting Started with Create React App
+# Jogo da Velha (TicTacToe) em React
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Este projeto implementa o clássico jogo da velha (TicTacToe) usando a biblioteca React. Permite que dois jogadores compitam para alcançar três símbolos iguais em uma linha, coluna ou diagonal no tabuleiro de jogo.
 
-## Available Scripts
+O tabuleiro do jogo é exibido na interface, permitindo que os jogadores cliquem nas células vazias para fazer suas jogadas. A interface também mostra o jogador atual e um menu que aparece quando o jogo termina, mostrando a vitória de um jogador ou o empate.
 
-In the project directory, you can run:
+## Funcionalidades Principais
 
-### `npm start`
+- Dois jogadores podem jogar alternadamente, representados pelos símbolos 'X' e 'O'.
+- O jogo detecta automaticamente se houve um vencedor (três símbolos iguais em uma linha, coluna ou diagonal) ou se houve um empate.
+- Os pontos são registrados e exibidos para cada jogador e para a quantidade de jogos jogados.
+- O jogo oferece a opção de reiniciar o tabuleiro para jogar novamente ou continuar jogando após o término de uma partida.
+- O layout responsivo e amigável facilita a jogabilidade em dispositivos diferentes.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+## Licença
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+Este projeto está sob a [Licença MIT](LICENSE).
--- src/App.js
+++ src/App.js
@@ -1,23 +1,177 @@
-import logo from './logo.svg';
 import './App.css';
+import { FaRegCircle, FaTimes } from 'react-icons/fa';
+import { useState, useEffect, useCallback } from 'react';
 
 function App() {
+  const [menuClass, setMenuClass] = useState('none');
+  const [menu, setMenu] = useState(false);
+  const [winner, setWinner] = useState(false);
+  const [player, setPlayer] = useState(["Player 1", <FaTimes className='player-1'/>]);
+  const [board, setBoard] = useState([null, null, null, null, null, null, null, null, null]);
+  const [points, setPoints] = useState([0, 0, 0])
+
+  useEffect(() => {
+    const savedPoints = localStorage.getItem("points");
+    if (savedPoints) {
+      setPoints(JSON.parse(savedPoints));
+    }
+  }, []);
+
+  const setLocalPoints = useCallback((points) => {
+    localStorage.setItem("points", JSON.stringify(points));
+    setPoints(points);
+  }, [setPoints]);
+
+  useEffect(() => {
+    if (winner === true) {
+      if (player[0] !== 'Velha') {
+        const newPoints = [...points]
+
+        if (player[0] === "Player 1") {
+          newPoints[0] += 1
+        } else {
+          newPoints[2] += 1
+        }
+        newPoints[1] += 1
+
+        setLocalPoints(newPoints)
+        setMenu(true)
+        setWinner(false)
+      }
+    };
+  }, [winner, player, points, setLocalPoints]);
+
+  useEffect(() => {
+    if (menu === true) {
+      setMenuClass('span');
+    } else {
+      setMenuClass('none');
+    };
+  }, [menu]);
+
+  function verifyWinner(newBoard) {
+    const winnerCases = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+    for (let i = 0; i < winnerCases.length; i++) {
+      const notNull = newBoard[winnerCases[i][0]] !== null && newBoard[winnerCases[i][1]] !== null && newBoard[winnerCases[i][2]] !== null;
+      if (notNull) {
+        if (newBoard[winnerCases[i][0]].props.className === newBoard[winnerCases[i][1]].props.className && newBoard[winnerCases[i][0]].props.className === newBoard[winnerCases[i][2]].props.className) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  function verifyDraw(newBoard) {
+    if (winner === false) {
+      for (let i = 0; i < 9; i++) {
+        if (newBoard[i] === null) {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
+
+  function handleClick(index) {
+    if (board[index] === null && winner === false) {
+      const newBoard = [...board];
+
+      if (player[0] === "Player 1") {
+        newBoard[index] = <FaTimes className='player-1' />;
+        if (verifyWinner(newBoard)) {
+          setWinner(true);
+        } else {
+          setPlayer(["Player 2", <FaRegCircle className='player-2' />]);
+        }
+
+      } else {
+        newBoard[index] = <FaRegCircle className='player-2' />;
+        if (verifyWinner(newBoard)) {
+          setWinner(true);
+        } else {
+          setPlayer(["Player 1", <FaTimes className='player-1' />]);
+        }
+      }
+      if (verifyDraw(newBoard)) {
+        const newPoints = [...points];
+        newPoints[1] += 1;
+
+        setLocalPoints(newPoints);
+        setPlayer(["Velha", <div><FaRegCircle className='player-2' /><FaTimes className='player-1' /></div>]);
+        setWinner(true);
+        setMenu(true);
+      }
+      setBoard(newBoard);
+    }
+  }
+
+  function nextGame(e) {
+    e.preventDefault();
+    setWinner(false);
+    setBoard([null, null, null, null, null, null, null, null, null]);
+    setMenu(false);
+  }
+
+  function reset(e) {
+    e.preventDefault();
+    setWinner(false);
+    setLocalPoints([0, 0, 0]);
+    setBoard([null, null, null, null, null, null, null, null, null]);
+    setMenu(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='top'>
+          <h2>TicTacToe</h2>
+      </div>
+      <div className='main'>
+        <div className='board'>
+          <div onClick={() => handleClick(0)}>{board[0]}</div>
+          <div onClick={() => handleClick(1)}>{board[1]}</div>
+          <div onClick={() => handleClick(2)}>{board[2]}</div>
+          <div onClick={() => handleClick(3)}>{board[3]}</div>
+          <div onClick={() => handleClick(4)}>{board[4]}</div>
+          <div onClick={() => handleClick(5)}>{board[5]}</div>
+          <div onClick={() => handleClick(6)}>{board[6]}</div>
+          <div onClick={() => handleClick(7)}>{board[7]}</div>
+          <div onClick={() => handleClick(8)}>{board[8]}</div>
+        </div>
+        <div className={menuClass}>
+          <div className='background'></div>
+          <div className='menu'>
+            <div className='winner'>
+              {player[0] !== 'Velha' ? <b>{player[0]} venceu!</b> : <b>{player[0]}!</b>}
+            </div>
+            <div className='winnerIcon'>{player[1]}</div>
+            <div className='buttons'>
+              <button onClick={(e) => reset(e)}><b>Reset</b></button>
+              <button onClick={(e) => nextGame(e)}><b>Continuar Jogo</b></button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='points'>
+        <div className='player player-x'>
+          <FaTimes className='player-1'/>
+          <p className='playerName'>Player 1</p>
+          <p className='playerPoints'>{points[0]}</p>
+        </div>
+        <div className='games'>
+          <div className='playing'>
+            <div>{player[0]}</div>
+            <div>{player[1]}</div>
+          </div>
+          <p className='gamesP'>Games</p>
+          <p className='quantityGames'>{points[1]}</p>
+        </div>
+        <div className='player player-0'>
+          <FaRegCircle className='player-2'/>
+          <p className='playerName'>Player 2</p>
+          <p className='playerPoints'>{points[2]}</p>  
+        </div>  
+      </div>      
     </div>
   );
 }
